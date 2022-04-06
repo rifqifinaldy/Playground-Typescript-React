853ed51724737dc44e7d63f259d62720725dafe9
rifqifinaldy <rifqifinaldy@gmail.com>
2022-04-06 13:39:20 +0700

    Configure New React 18 with TS supprt

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [barang, setBarang] = useState([
+    {
+      nama: "Kardus",
+      jumlah: 2,
+      tersedia: true,
+      keterangan: "Kardus Milik Semua Orang",
+    },
+    {
+      nama: "Kardus Bekas",
+      jumlah: 3,
+      tersedia: true,
+    },
+  ]);
+
+  barang.map(barang => {
+    barang.jumlah = 4
+  })
+
+  console.log(barang)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Gudang App</h1>
     </div>
   );
 }
